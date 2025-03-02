6dbcf96c3c77d3e4dc61f1b841c1cf5033cda528
FullSol <ceraines@student.fullsail.edu>
2025-03-02 12:56:53 -0500

    separated styles for readability in chat container

--- src/features/chat/ChatContainer.js
+++ src/features/chat/ChatContainer.js
@@ -3,25 +3,24 @@ import { Box } from "@mui/material";
 import ChatButton from "./ChatButton";
 import ChatWindow from "./ChatWindow";
 
+const chatContainerStyles = {
+  width: "400px",
+  margin: "auto",
+  position: "absolute",
+  display: "flex",
+  justifyContent: "right",
+  bottom: "20px",
+  right: "20px",
+  p: 2,
+  borderRadius: 1,
+};
+
 const ChatContainer = ({ messages, sendMessage }) => {
   const [isChatVisible, setIsChatVisible] = useState(false);
 
   return (
     //Wrapper for everything chat related
-    <Box
-      component="section"
-      sx={{
-        width: "400px",
-        margin: "auto",
-        position: "absolute",
-        display: "flex",
-        justifyContent: "right",
-        bottom: "20px",
-        right: "20px",
-        p: 2,
-        borderRadius: 1,
-      }}
-    >
+    <Box component="section" sx={{ chatContainerStyles }}>
       {/* Toggle button for chat */}
       {!isChatVisible && (
         <ChatButton toggleChat={() => setIsChatVisible((prev) => !prev)} />
